02a6d306370f4b9edd0743c548563fc3e268341e
venice18 <carolinestephen07@gmail.com>
2024-02-29 14:00:18 +0300

    first commit

---
+++ DATATYPES/Data_Types.py
@@ -0,0 +1,37 @@
+a=100
+b=3.14
+print(a*b)
+c=5+7j
+print(a+b+c)
+d=True
+print(a*b+c-d)
+str = 'Welcome to PLP'
+print(str)
+print(str[0])
+print(str[-1])
+print(str[4:10])
+plp ='Python Class'
+print(str + plp)
+print(plp * 4)
+Shopping_list =['bread','milk',20,30,40.5]
+print(Shopping_list)
+Building_list =[450, 'wheelbarrows', 180, "shovels", 'bread', 'milk']
+print(Building_list)
+print(Shopping_list[0])
+print(Shopping_list[4:5])
+print(Shopping_list +Building_list)
+print(Shopping_list * 2)
+plp_tuple = 'abcd', 786 , 2.23, 'john', 70.2
+print(plp_tuple)
+tiny_tuple =('John', 5+7j)
+print(tiny_tuple)
+print(plp_tuple + tiny_tuple)
+dict ={}
+dict['one'] ="This is one"
+dict[2] = "This is two"
+dict[3] = "This is three"
+print(dict)
+tinydict ={'name':'john','code':'6343'}
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
