3fd805a85efa3aed7bfc95a95f2a26cee44007d3
venice18 <carolinestephen07@gmail.com>
2024-03-06 14:15:12 +0300

    second commit

--- DATATYPES/main.dart
+++ DATATYPES/main.dart
@@ -1,1 +1,23 @@
-
+void main() {
+//int
+  int num1 = 10;
+  //double
+  double num2 = 5.5;
+  print(num1);
+  print(num2);
+  String school = "Power Learn Project";
+  String address = "Mombasa";
+  print("Hey there I am Liz a student at $school and i come from $address");
+  bool isgraduated = true;
+  print("graduation status :$isgraduated");
+  var cars = ["BMW", "Audi", "Benz", "Ferrari"];
+  print(cars);
+  print(cars[0]);
+  Map details = {
+    "Eliza": 18,
+    "Esther": 16,
+    "David": 20,
+  };
+  print(details);
+  String runes = "\u{1F600}";
+}
---
+++ DATATYPES/my_own_datatypes.py
@@ -0,0 +1,44 @@
+str = 'welcome to my own datatypes coding'
+print(str)
+print(str[0])
+print(str[4])
+home = 'This one of my best times getting time to code alone'
+print(home)
+print(str + home)
+b = 120
+y = 300.542
+print(b + y)
+print(b *y)
+c = 59+3j
+print(b + y - c )
+print(c - b * y)
+Packing_list = ('books', 'writing materials', 'dish', 'spoon', 20 ,35 ,45.32)
+print(Packing_list)
+print(Packing_list[4])
+Church_list = ('speakers', 'powerpoints', 'camera', 'laptops', 35 ,29.34)
+print(Church_list)
+print(Packing_list + Church_list)
+my_tuple = 'Liz' , 18 , 'Joseph' , 8.32
+print(my_tuple)
+tiny_tuple = 'Esther' , 16.32
+print(tiny_tuple)
+print(my_tuple + tiny_tuple)
+print(tiny_tuple + my_tuple + Packing_list + Church_list)
+dict = {}
+dict[1] = 'This is one'
+dict[5] = 'This is five'
+dict['Joe'] = 'This is Joseph'
+dict['Essie'] = 'This is Esther'
+dict['Mama'] = 'This is Caroline'
+dict['Dad'] = 'This is Steve'
+print(dict)
+little_dict ={'my name':'Liz Steve', 'my age':'18'}
+print(little_dict)
+print(little_dict.keys())
+print(little_dict.values())
+
+
+
+
+
+
